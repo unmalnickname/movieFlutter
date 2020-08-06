b82607081ba61f9d38617ed39b299941b77b7e43
Luis Avila <a4288177@gmail.com>
2020-08-06 00:03:52 -0400

    098 Widget personalizado finalizado

--- lib/src/widgets/cards_swiper_widget.dart
+++ lib/src/widgets/cards_swiper_widget.dart
@@ -5,19 +5,24 @@ class CardSwiper extends StatelessWidget {
   final List<dynamic> peliculas;
 
   CardSwiper({@required this.peliculas});
+
   @override
   Widget build(BuildContext context) {
+    final _screenSize = MediaQuery.of(context).size;
+
     return Container(
       padding: EdgeInsets.only(top: 10.0),
-      width: double.infinity,
-      height: 300.0,
       child: Swiper(
         layout: SwiperLayout.STACK,
-        itemWidth: 350.0,
+        itemWidth: _screenSize.width * 0.7,
+        itemHeight: _screenSize.height * 0.5,
         itemBuilder: (BuildContext context, int index) {
-          return new Image.network(
-            "http://via.placeholder.com/350x150",
-            fit: BoxFit.fill,
+          return ClipRRect(
+            borderRadius: BorderRadius.circular(20.0),
+            child: new Image.network(
+              "http://via.placeholder.com/350x150",
+              fit: BoxFit.cover,
+            ),
           );
         },
         itemCount: 3,
